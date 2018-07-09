3760e5a8cbe1409b4d9161f1415a37202fc70e5c
eders <eders@DESKTOP-3LOO78O>
2018-07-09 20:48:34 -0300

    Colocando Projeto No Git.

---
+++ src/main/java/br/com/concrete/ConcreteDesafioGradleApplication.java
@@ -0,0 +1,12 @@
+package br.com.concrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcreteDesafioGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcreteDesafioGradleApplication.class, args);
+	}
+}
